c742c84efadc039d27a7f2526cbe818807f39507
seriouszyx <674965440@qq.com>
2019-03-13 13:58:08 +0800

    Add assignment #1: Warmsup

---
+++ Assignment1-codes/CenteredText.java
@@ -0,0 +1,16 @@
+/*
+ * File: CenteredText.java
+ * -----------------------
+ * This programs displays a message centered in the graphics window.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+
+public class CenteredText extends GraphicsProgram {	
+
+	public void run() {
+		setSize(800, 600);
+		// You fill the codes here
+	}
+}
---
+++ Assignment1-codes/RobotFace.java
@@ -0,0 +1,29 @@
+/*
+ * File: RobotFace.java
+ * --------------------
+ * This program draws a robot face using GRects and GOvals, centered
+ * in the graphics window.  We make sure to define constants at the
+ * top of our program instead of using magic numbers. We also write
+ * the program in terms of reusable and general methods
+ * drawRectangle and drawCircle.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+
+public class RobotFace extends GraphicsProgram {
+
+	/* Constants for the drawing */ 
+	private static final int HEAD_WIDTH = 150; 
+	private static final int HEAD_HEIGHT = 250; 
+	private static final int EYE_RADIUS = 20; 
+	private static final int MOUTH_WIDTH = 100; 
+	private static final int MOUTH_HEIGHT = 30; 
+	
+	public void run() {
+		setSize(600, 400);
+		// You fill the codes here
+	}
+
+}
