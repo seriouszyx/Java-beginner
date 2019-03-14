86c121f1a5f4afdca1c2b35ce22d16a52b9715f6
seriouszyx <674965440@qq.com>
2019-03-14 23:35:04 +0800

    Change files structure and merge cq's codes

--- Assignment1-codes/CenteredText.java
+++ Assignment1-codes/CenteredText.java
@@ -4,9 +4,6 @@
  * This programs displays a message centered in the graphics window.
  */
 
-import java.awt.Graphics;
-import java.awt.Frame;
-import java.awt.Toolkit;
 import acm.graphics.*;
 import acm.program.*;
 
@@ -15,14 +12,5 @@ public class CenteredText extends GraphicsProgram {
 	public void run() {
 		setSize(800, 600);
 		// You fill the codes here
-		GLabel label = new GLabel("Programming is Awesome!");
-	    label.setFont("Courier-52");
-	    add(label, 10, centerY);
 	}
-	public static void main(String[] args)
-	{
-		Frame frame = new Frame("?");
-		frame.setVisible(true);
-	}
-	
 }
